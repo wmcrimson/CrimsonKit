4152d0628e36eaafe557982730180dcb18aa7e39
Waqar Malik <wmalik@crimsonresearch.net>
2013-09-28 15:10:27 -0700

    Added Umbrella header file.

---
+++ CrimsonKit.h
@@ -0,0 +1,16 @@
+//
+//  CrimsonKit.h
+//
+//  Created by Waqar Malik on 9/28/13.
+//  Copyright (c) 2013 Crimson Research, Inc. All rights reserved.
+//
+
+#ifndef CrimsonKit_h
+#define CrimsonKit_h
+
+#import "CKLog.h"
+#import "CKSourceAnnotations.h"
+#import "ColorConversion.h"
+#import "UIColor+CrimsonKit.h"
+
+#endif
